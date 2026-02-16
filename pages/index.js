10ff4a041312cbb307ffdece49af1e4361a75097
Flapy93 <arram88888@gmail.com>
2026-02-16 17:16:15 +0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      fontFamily: "Arial",
+      padding: "40px",
+      background: "#0b0f1a",
+      color: "white",
+      height: "100vh"
+    }}>
+      <h1>Flapy</h1>
+      <p>Платформа для риэлторов</p>
+      <p>Система запущена</p>
+    </div>
+  );
+}
